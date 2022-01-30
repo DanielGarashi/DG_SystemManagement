a9a7f5f66849e00e8d02e0c2daf5447da3f2f7ce
Daniel Gerashi <danielgarashi@gmail.com>
2022-01-30 23:50:09 +0200

    After move all the Indication msg for a separate class

---
+++ src/main/java/danielgarashi/DG_SystemManagement/entity/IndicationMsg.java
@@ -0,0 +1,38 @@
+package danielgarashi.DG_SystemManagement.entity;
+
+public class IndicationMsg {
+    public static final String SERVER_GENERAL_ERROR_ENG = "Unknown error, please try again.";
+    public static final String SERVER_GENERAL_ERROR_HEB = "קרתה תקלה לא ידועה, אנא נסה שוב.";
+
+    /*AuthenticationService class*/
+    public static final String USER_EXISTS_ERROR_ENG = "This user already in use, please try again.";
+    public static final String USER_EXISTS_ERROR_HEB = "משתמש קיים, אנא נסה שנית.";
+
+    public static final String INVALID_PARAM_ERROR_ENG = "One or more of the parameters you entered are invalid.";
+    public static final String INVALID_PARAM_ERROR_HEB = "שדה אחד או יותר אינו תקין.";
+
+    public static final String UPDATE_STUDENT_DETAILS_SUCCESS_ENG = "Your details have been successfully updated!.";
+    public static final String UPDATE_STUDENT_DETAILS_SUCCESS_HEB = "פרטיך עודכנו בהצלחה!.";
+
+    /*StudentService class*/
+    public static final String COURSE_STARTED_ERROR_ENG = "The course has already started, you can not register";
+    public static final String COURSE_STARTED_ERROR_HEB = "הקורס כבר התחיל, לא ניתן להירשם.";
+
+    public static final String STUDENT_REGISTERED_COURSE_ERROR_HEB = "You already registered to this course!.";
+    public static final String STUDENT_REGISTERED_COURSE_HEB = "הינך רשום לקורס זה, לא ניתן להירשם שוב.";
+
+    public static final String CANCEL_COURSE_SUCCESS_ENG = "Course cancellation completed successfully.";
+    public static final String CANCEL_COURSE_SUCCESS_HEB = "ביטול הקורס בוצע בהצלחה.";
+
+    public static final String STUDENT_CANCEL_STARTED_COURSE_ERROR_ENG = "The course already started, you can't cancel the registration";
+    public static final String STUDENT_CANCEL_STARTED_COURSE_ERROR_HEB = "הקורס כבר התחיל, לא ניתן לבטל רישום.";
+
+    public static final String COURSE_NOT_EXISTS_ERROR_ENG = "Course doesn't exists, please try again.";
+    public static final String COURSE_NOT_EXISTS_ERROR_HEB = "הקורס אינו קיים, אנא נסה שוב.";
+
+    public static final String STUDENT_COURSE_NOT_EXISTS_ERROR_ENG = "You are not registered for this course, please try again.";
+    public static final String STUDENT_COURSE_NOT_EXISTS_ERROR_HEB = "אינך רשום לקורס זה, אנא נסה שוב.";
+
+    public static final String STUDENT_NOT_EXISTS_ERROR_ENG = "Student not exists, please try again.";
+    public static final String STUDENT_NOT_EXISTS_ERROR_HEB = "סטודנט לא קיים, אנא נסה שוב.";
+}
